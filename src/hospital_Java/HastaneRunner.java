d8fe9d0f7690f4022aa69971ff95584e1f641a76
Ustun Ertas <ustunertas0@gmail.com>
2023-03-20 20:08:15 +0300

    new project

---
+++ src/hospital_Java/HastaneRunner.java
@@ -0,0 +1,13 @@
+package hospital_Java;
+
+public class HastaneRunner {
+    public static void main(String[] args) {
+
+        System.out.println("====================   JAVA\tHASTANESİNE HOSGELDINIZ  =========================");
+        VeriBankasi.hastanınGirisZamanı();
+        Hasta basla= new Hasta();
+        basla.hastaBilgisiOlustur();
+
+
+    }
+}
